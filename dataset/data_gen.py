141313200aeb801da48f73a1c6208ed1c56ab010
preritj <prerit.jaiswal@gmail.com>
2018-04-17 12:56:26 -0700

    data pipeline

--- dataset/data_gen.py
+++ dataset/data_gen.py
@@ -9,83 +9,10 @@ import functools
 slim_example_decoder = tf.contrib.slim.tfexample_decoder
 
 
-def _decoder():
-    keys_to_features = {
-        'image/filename':
-            tf.FixedLenFeature((), tf.string, default_value=''),
-        'image/shape':
-            tf.FixedLenFeature([2], tf.int64),
-        'image/num_instances':
-            tf.FixedLenFeature((), tf.int64),
-        'image/person/bbox':
-            tf.VarLenFeature(tf.float32),
-        'image/person/keypoints':
-            tf.VarLenFeature(tf.float32),
-        'image/mask/x':
-            tf.VarLenFeature(tf.int64),
-        'image/mask/y':
-            tf.VarLenFeature(tf.int64)
-    }
-    items_to_handlers = {
-        'image': slim_example_decoder.ItemHandlerCallback(
-            'image/filename', _image_decoder),
-        'mask': slim_example_decoder.ItemHandlerCallback(
-            ['image/mask/x', 'image/mask/y', 'image/shape'],
-            _mask_decoder),
-        'keypoints': slim_example_decoder.ItemHandlerCallback(
-            ['image/person/keypoints', 'image/num_instances'],
-            _keypoints_decoder)
-    }
-    decoder = slim_example_decoder.TFExampleDecoder(keys_to_features,
-                                                    items_to_handlers)
-    return decoder
-
-
-def _image_decoder(keys_to_tensors):
-    filename = keys_to_tensors['image/filename']
-    image_string = tf.read_file(filename)
-    image_decoded = tf.image.decode_jpeg(image_string, channels=3)
-    # image_decoded = tf.image.resize_images(image_decoded, size=[300, 300])
-    return image_decoded
-
-
-def _mask_decoder(keys_to_tensors):
-    mask_x = keys_to_tensors['image/mask/x']
-    mask_y = keys_to_tensors['image/mask/y']
-    shape = keys_to_tensors['image/shape']
-    if isinstance(shape, tf.SparseTensor):
-        shape = tf.sparse_tensor_to_dense(shape)
-    if isinstance(mask_x, tf.SparseTensor):
-        mask_x = tf.sparse_tensor_to_dense(mask_x)
-    if isinstance(mask_y, tf.SparseTensor):
-        mask_y = tf.sparse_tensor_to_dense(mask_y)
-    indices = tf.stack([mask_x, mask_y], axis=1)
-    values = tf.ones_like(mask_x)
-    mask = tf.SparseTensor(indices=indices, values=values,
-                           dense_shape=shape)
-    mask = tf.sparse_tensor_to_dense(mask, default_value=0)
-    #mask.set_shape([None, None])
-    #mask = tf.image.resize_images(mask, size=[300, 300])
-    return mask
-
-
-def _keypoints_decoder(keys_to_tensor):
-    keypoints = keys_to_tensor['image/person/keypoints']
-    num_instances = keys_to_tensor['image/num_instances']
-    if isinstance(num_instances, tf.SparseTensor):
-        num_instances = tf.sparse_tensor_to_dense(num_instances)
-    shape = [num_instances] + [15, 3]
-    if isinstance(keypoints, tf.SparseTensor):
-        keypoints = tf.sparse_reshape(keypoints, shape=shape)
-        keypoints = tf.sparse_tensor_to_dense(keypoints)
-    else:
-        keypoints = tf.reshape(keypoints, shape=shape)
-    return keypoints
-
-
 class PoseDataReader(object):
-    def __init__(self):
+    def __init__(self, num_keypoints=15):
         self.datasets = []
+        self.num_keypoints = num_keypoints
 
     def add_dataset(self, name, img_dir, tfrecord_path,
                     annotation_files=None, weight=1.,
@@ -116,10 +43,100 @@ class PoseDataReader(object):
         probs = np.array(probs)
         return probs / np.sum(probs)
 
+    @staticmethod
+    def _get_tensor(tensor):
+        if isinstance(tensor, tf.SparseTensor):
+            return tf.sparse_tensor_to_dense(tensor)
+        return tensor
+
+    @staticmethod
+    def _image_decoder(keys_to_tensors):
+        filename = keys_to_tensors['image/filename']
+        image_string = tf.read_file(filename)
+        image_decoded = tf.image.decode_jpeg(image_string, channels=3)
+        return image_decoded
+
+    @staticmethod
+    def _mask_decoder(keys_to_tensors):
+        mask_x = PoseDataReader._get_tensor(
+            keys_to_tensors['image/mask/x'])
+        mask_y = PoseDataReader._get_tensor(
+            keys_to_tensors['image/mask/y'])
+        shape = PoseDataReader._get_tensor(
+            keys_to_tensors['image/shape'])
+        indices = tf.stack([mask_x, mask_y], axis=1)
+        values = tf.ones_like(mask_x)
+        mask = tf.SparseTensor(indices=indices, values=values,
+                               dense_shape=shape)
+        mask = tf.sparse_tensor_to_dense(mask, default_value=0)
+        return mask
+
+    @staticmethod
+    def _keypoints_decoder(keys_to_tensor, num_keypoints=15):
+        keypoints = keys_to_tensor['image/person/keypoints']
+        num_instances = PoseDataReader._get_tensor(
+            keys_to_tensor['image/num_instances'])
+        shape = [num_instances] + [num_keypoints, 3]
+        if isinstance(keypoints, tf.SparseTensor):
+            keypoints = tf.sparse_reshape(keypoints, shape=shape)
+            keypoints = tf.sparse_tensor_to_dense(keypoints)
+        else:
+            keypoints = tf.reshape(keypoints, shape=shape)
+        return keypoints
+
+    @staticmethod
+    def _bbox_decoder(keys_to_tensor):
+        bbox = keys_to_tensor['image/person/bbox']
+        num_instances = PoseDataReader._get_tensor(
+            keys_to_tensor['image/num_instances'])
+        shape = [num_instances] + [4]
+        if isinstance(bbox, tf.SparseTensor):
+            bbox = tf.sparse_reshape(bbox, shape=shape)
+            bbox = tf.sparse_tensor_to_dense(bbox)
+        else:
+            bbox = tf.reshape(bbox, shape=shape)
+        return bbox
+
+    def _decoder(self):
+        keys_to_features = {
+            'image/filename':
+                tf.FixedLenFeature((), tf.string, default_value=''),
+            'image/shape':
+                tf.FixedLenFeature([2], tf.int64),
+            'image/num_instances':
+                tf.FixedLenFeature((), tf.int64),
+            'image/person/bbox':
+                tf.VarLenFeature(tf.float32),
+            'image/person/keypoints':
+                tf.VarLenFeature(tf.float32),
+            'image/mask/x':
+                tf.VarLenFeature(tf.int64),
+            'image/mask/y':
+                tf.VarLenFeature(tf.int64)
+        }
+        keypoints_decoder = functools.partial(
+            self._keypoints_decoder, num_keypoints=self.num_keypoints)
+        items_to_handlers = {
+            'image': slim_example_decoder.ItemHandlerCallback(
+                'image/filename', self._image_decoder),
+            'mask': slim_example_decoder.ItemHandlerCallback(
+                ['image/mask/x', 'image/mask/y', 'image/shape'],
+                self._mask_decoder),
+            'keypoints': slim_example_decoder.ItemHandlerCallback(
+                ['image/person/keypoints', 'image/num_instances'],
+                keypoints_decoder),
+            'bbox': slim_example_decoder.ItemHandlerCallback(
+                ['image/person/bbox', 'image/num_instances'],
+                self._bbox_decoder)
+        }
+        decoder = slim_example_decoder.TFExampleDecoder(keys_to_features,
+                                                        items_to_handlers)
+        return decoder
+
     def read_data(self, train_config):
         probs = self._get_probs()
         probs = tf.cast(probs, tf.float32)
-        decoder = _decoder()
+        decoder = self._decoder()
         filenames = [ds['tfrecord_path'] for ds in self.datasets]
         file_ids = list(range(len(filenames)))
         dataset = tf.data.Dataset.from_tensor_slices((file_ids, filenames))
@@ -143,6 +160,8 @@ class PoseDataReader(object):
         if train_config.shuffle:
             records_dataset.shuffle(train_config.shuffle_buffer_size)
         # records_dataset = records_dataset.batch(config.batch_size)
+        decode_fn = functools.partial(
+            decoder.decode, items=['image', 'keypoints', 'bbox', 'mask'])
         tensor_dataset = records_dataset.map(
-            decoder.decode, num_parallel_calls=train_config.num_parallel_map_calls)
+            decode_fn, num_parallel_calls=train_config.num_parallel_map_calls)
         return tensor_dataset.prefetch(train_config.prefetch_size)
